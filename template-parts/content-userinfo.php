48a7d2f25c2a560913c03023dd0078b81341c1fc
Marcos Imfeld <sebastianimfeld@gmail.com>
2020-06-25 20:14:06 -0300

    finish v1

--- template-parts/content-userinfo.php
+++ template-parts/content-userinfo.php
@@ -1,26 +1,15 @@
 <div class="row d-flex align-items-center shadow-sm bg-white py-3 mb-3">
     <div class="col text-center pr-0 pr-lg-0">
-        <?php
-            $user = wp_get_current_user();
-            if ( $user ) :
-        ?>
         <figure>
-            <img class="rounded-sm rounded-circle" style="width: 100px;" src="<?php echo esc_url( get_avatar_url( $user->ID ) ); ?>" />
+            <img class="rounded-sm rounded-circle" style="width: 100px;" src="wp-content/themes/marcostheme/assests/images/avatar-marcos.jpeg" />
         </figure>
-        <?php endif; ?>
     </div>
     <div class="col-8 col-md-9 col-lg-10 pl-0 pl-md-0">
         <p class="h4 font-weight-bold"> 
-            <?php global $current_user;
-                get_currentuserinfo();
-                echo $current_user->display_name;
-            ?>
+            Marcos Imfeld
         </p>
         <p class="text-muted">
-            <?php global $current_user;
-                get_currentuserinfo();
-                echo $current_user->user_description;
-            ?>
+            Soy estudiante de Ing. en Sistemas. Hago videos de python, django, web development.
         </p>
         <p>
             <a href="https://github.com/markimfeld" target="_blank" class="text-info mr-1"><i class="fab fa-github"></i></a>
